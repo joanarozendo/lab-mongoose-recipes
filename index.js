daad8a697ae2034bdb4b79213c9c50b318d8bf85
Joana Rozendo <joanarozendo@gmail.com>
2019-11-14 18:56:20 +0000

    iterations 1 through 5 completed

--- index.js
+++ index.js
@@ -1,19 +1,61 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import Recipe model
-const Recipe = require('./models/Recipe');
+const Recipe = require("./models/Recipe");
 
 // Import data
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost/recipeApp';
+const MONGODB_URI = "mongodb://localhost/recipeApp";
 
 // Connection to the database "recipeApp"
 mongoose
   .connect(MONGODB_URI, { useNewUrlParser: true })
   .then(() => {
-    console.log('Connected to Mongo!');
+    console.log("Connected to Mongo!");
+    Recipe.create({
+      title: "Veggie Lasagna",
+      level: "Amateur Chef",
+      ingredients: ["pasta", "tomato sauce", "vegetables", "cream"],
+      cuisine: "Italian",
+      dishType: "Dish",
+      image:
+        "https://www.inspiredtaste.net/wp-content/uploads/2016/10/Easy-Vegetable-Lasagna-Recipe-1200.jpg",
+      duration: 60,
+      creator: "Luigi"
+    })
+      .then(createdRecipe => {
+        console.log("We successfully created a recipe.");
+        // console.log(createdRecipe);
+        console.log(createdRecipe.title);
+        return Recipe.insertMany(data);
+      })
+      .then(recipeArray => {
+        console.log(
+          "We successfully added the array of recipes to the database."
+        );
+        for (i = 0; i < recipeArray.length; i++) {
+          console.log(recipeArray[i].title);
+        }
+        console.log(recipeArray);
+        
+        return Recipe.updateOne({ title: 'Rigatoni alla Genovese' }, { duration: 110 });
+      })
+      .then(changedTitledDocument => {
+        console.log(changedTitledDocument);
+        console.log('Success! Changed the duration of the Rigatoni alla Genovese recipe.');
+        return Recipe.deleteOne({ title: 'Carrot Cake' });
+      })
+      .then(deletedRecipeDocument => {
+        console.log(deletedRecipeDocument);
+        console.log('Success! Deleted the Carrot Cake recipe.');
+        
+      })
+      .catch(error => {
+        console.log("There was an error in the chain of operations.");
+        console.log(error);
+      });
   })
   .catch(err => {
-    console.error('Error connecting to mongo', err);
+    console.error("Error connecting to mongo", err);
   });
